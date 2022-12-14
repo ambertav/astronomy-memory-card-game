d8e81361ff3fd3ca699777e26151f74e9c6cbc26
Amber Taveras <taveras.amber@gmail.com>
2022-12-14 18:55:10 -0500

    WIP: rework diff. change func

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
     const $section = $('section');
 
-    // event listeners toggle difficulty
-    $('#easy').on('click', function (evt) {
-        $section.attr('class', 'gallery4');
-    });
+    // change difficulty
+    $('#easy').on('click', changeGrid);
+    $('#med').on('click', changeGrid);
+    $('#hard').on('click', changeGrid);
 
-    $('#med').on('click', function (evt) {
-        $section.attr('class', 'gallery5');
-    });
 
-    $('#hard').on('click', function (evt) {
-        $section.attr('class', 'gallery6');
-    });
-
+    // change difficulty
+    function changeGrid(evt) {
+        if ($(evt.target).attr('id') === 'easy') {
+            $section.attr('class', 'gallery4');
+        }
+        if ($(evt.target).attr('id') === 'med') {
+            $section.attr('class', 'gallery5');
+        }
+        if ($(evt.target).attr('id') === 'hard') {
+            $section.attr('class', 'gallery6');
+        }
+    }   
 
 });
+
