ee5e936fffbbfece4cb726e783f931fa70b4af62
Amber Taveras <taveras.amber@gmail.com>
2022-12-17 12:12:52 -0500

    WIP: timeout hide, shuffle & border img

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
 
     let moves = 0;
     let matches = 0;
-    let accuracy = matches / moves;
     const $flippedImages = $([])
     const matchArray = [];
 
@@ -22,7 +21,6 @@ $(document).ready(function () {
     // render images in pairs
     $start.on('click', renderGrid);
 
-
     $('img').on('click', showImages);
 
     // ajax, render images in pairs
@@ -31,16 +29,17 @@ $(document).ready(function () {
             url: "https://api.nasa.gov/planetary/apod?api_key=hIlNblkyFJyGSZstYSaXgPk0m9o3WKjNpP1iHb6F&count=9&concept_tags=True"
         }).then(
             function (data) {
-                // shuffleIds(classWithPair);
+                shuffleClass(classWithPair);
                     $gridArray.each(function (index) {
                         $(`#${index + 1}`).attr('class', classWithPair[index]);
                     });
                         for (let i = 1; i < 9; i++) {
                             $(data).each(function () {
                             $(`.${i}`).attr('src', data[i].url);
-                        });
-                               
-            } hideImages($('img'));
+                        });       
+            } setTimeout (function (){
+                hideImages($('img'));
+            }, 2000);
             gameRound();
         },
             function (error) {
@@ -48,14 +47,18 @@ $(document).ready(function () {
             }
             )};
     
-    // hide images immediately after rendering with ajax
+    // hide images rendering with ajax
     function hideImages(img) {
-        $(img).addClass('hide');
+        setTimeout(function () {
+            $(img).addClass('hide');
+        }, 1000);
     }
+
 
     // show images with click
     function showImages(evt) {
         $(evt.target).removeClass('hide');
+        $(evt.target).css('box-shadow', '0px 0px 30px yellow');
         $flippedImages.push($(evt.target));
         gameRound();
     }
@@ -64,10 +67,12 @@ $(document).ready(function () {
         gameEnd();
         if ($flippedImages.length === 2) {
             moves += 1;
-            $('.moves').html(`Moves: ${moves}`);
+            $('#moves').html(`Moves: ${moves}`);
             if ($flippedImages.eq(0)[0].attr('class') === $flippedImages.eq(1)[0].attr('class')) {
                 matches += 1;
-                $('.accuracy').html(`Accuracy: ${accuracy}`);
+                $('#matches').html(`Matches: ${matches}`);
+                let accuracy = `${Math.round((matches / moves) *100)}%`; 
+                $('#acc').html(`Accuracy: ${accuracy}`);
                 matchArray.push($flippedImages.splice(0, 2));
         } else {
             hideImages($flippedImages[0]);
@@ -77,12 +82,13 @@ $(document).ready(function () {
         }
     }
 
+    // function checkMatch () {
+    // }
+
+
     function gameEnd () {
         if (matchArray.length === cls.length) {
             alert('Congratulations, you win!');
-            $start.off()
-        } else {
-            return;
         }
     }
 
@@ -98,21 +104,20 @@ $(document).ready(function () {
             $section.attr('class', 'gallery24');
         }
     }
-})   
 
     // Fisher–Yates Shuffle
-//     function shuffleIds (array) {
-//         let m = array.length;
-//         let t;
-//         let i;
+    function shuffleClass (array) {
+        let m = array.length;
+        let t;
+        let i;
 
-//         while (m) {
-//             i = Math.floor(Math.random() * m--);
-//             t = array[m];
-//             array[m] = array[i];
-//             array[i] = t;
-//         }
-//         return array;
-//     }
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+        return array;
+    }
+})   
 
-// });
