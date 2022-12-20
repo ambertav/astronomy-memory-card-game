d4a9cc9a298f1276a66ef60c2e55ae0d211bb6c7
Amber Taveras <taveras.amber@gmail.com>
2022-12-19 21:44:08 -0500

    remove diff. finish game logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     const $section = $('section');
     const $start = $('button');
+    const $dialogue = $('#dialogue');
 
     let $grid = $(document.querySelectorAll('img'));
     const $gridArray = $(Array.from($grid));
@@ -11,106 +12,97 @@ $(document).ready(function () {
     let moves = 0;
     let matches = 0;
     const $flippedImages = $([])
-    const matchArray = [];
-
-    // change difficulty
-    $('#easy').on('click', changeGrid);
-    $('#med').on('click', changeGrid);
-    $('#hard').on('click', changeGrid);
+    let matchArray = [];
 
     // render images in pairs
     $start.on('click', renderGrid);
-
+    
     $('img').on('click', showImages);
-
+    
     // ajax, render images in pairs
     function renderGrid() {
+        matchArray = [];
+        moves = 0;
+        matches = 0;
+        $('#acc').html(`Accuracy: 0%`);
+        $dialogue.html('Get ready...');
         $.ajax({
-            url: "https://api.nasa.gov/planetary/apod?api_key=hIlNblkyFJyGSZstYSaXgPk0m9o3WKjNpP1iHb6F&count=9&concept_tags=True"
+            url: "https://api.nasa.gov/planetary/apod?api_key=hIlNblkyFJyGSZstYSaXgPk0m9o3WKjNpP1iHb6F&count=9"
         }).then(
             function (data) {
                 shuffleClass(classWithPair);
-                    $gridArray.each(function (index) {
-                        $(`#${index + 1}`).attr('class', classWithPair[index]);
-                    });
-                        for (let i = 1; i < 9; i++) {
-                            $(data).each(function () {
-                            $(`.${i}`).attr('src', data[i].url);
-                        });       
-            } setTimeout (function (){
-                hideImages($('img'));
-            }, 2000);
-            gameRound();
-        },
+                $gridArray.each(function (index) {
+                    $(`#${index + 1}`).attr('class', classWithPair[index]);
+                });
+                for (let i = 1; i < 9; i++) {
+                    $(data).each(function () {
+                        $(`.${i}`).attr('src', data[i].url);
+                    });       
+                } setTimeout (function (){
+                    hideImages($('img'));
+                }, 3000);
+                gameRound();
+            },
             function (error) {
                 alert('error');
             }
             )};
-    
-    // hide images rendering with ajax
+            
     function hideImages(img) {
         setTimeout(function () {
             $(img).addClass('hide');
-        }, 1000);
+            $dialogue.html('Click on two images');
+            }, 750);
     }
-
-
-    // show images with click
+            
     function showImages(evt) {
         $(evt.target).removeClass('hide');
         $(evt.target).css('box-shadow', '0px 0px 30px yellow');
         $flippedImages.push($(evt.target));
         gameRound();
-    }
-
+        }
+            
     function gameRound() {
-        gameEnd();
         if ($flippedImages.length === 2) {
             moves += 1;
-            $('#moves').html(`Moves: ${moves}`);
             if ($flippedImages.eq(0)[0].attr('class') === $flippedImages.eq(1)[0].attr('class')) {
                 matches += 1;
-                $('#matches').html(`Matches: ${matches}`);
-                let accuracy = `${Math.round((matches / moves) *100)}%`; 
-                $('#acc').html(`Accuracy: ${accuracy}`);
+                $flippedImages.each(function (index) {
+                    $flippedImages.eq(index)[0].css('box-shadow', '0px 0px 40px #39FF14');
+                });
+                $dialogue.html('It\'s a match!');
                 matchArray.push($flippedImages.splice(0, 2));
-        } else {
-            hideImages($flippedImages[0]);
-            hideImages($flippedImages[1]);
-            $flippedImages.splice(0, 2);
+            } else {
+                $dialogue.html('Not a match... try again');
+                hideImages($flippedImages[0]);
+                hideImages($flippedImages[1]);
+                $flippedImages.splice(0, 2);
+            }
         }
-        }
+        gameUpdate();
     }
-
-    // function checkMatch () {
-    // }
-
-
+            
+    function gameUpdate() {
+        $('#moves').html(`Moves: ${moves}`);
+        $('#matches').html(`Matches: ${matches}`);
+        let accuracy = `${Math.round((matches / moves) * 100)}%`;
+        if (accuracy !== 'NaN%') {
+            $('#acc').html(`Accuracy: ${accuracy}`);
+        }
+        gameEnd();
+    }
+            
     function gameEnd () {
         if (matchArray.length === cls.length) {
-            alert('Congratulations, you win!');
+            $dialogue.html('Congratulations, you win!<br><br><br>Click start game to play again');    
         }
     }
-
-    // change difficulty
-    function changeGrid(evt) {
-        if ($(evt.target).attr('id') === 'easy') {
-            $section.attr('class', 'gallery16');
-        }
-        if ($(evt.target).attr('id') === 'med') {
-            $section.attr('class', 'gallery20');
-        }
-        if ($(evt.target).attr('id') === 'hard') {
-            $section.attr('class', 'gallery24');
-        }
-    }
-
+               
     // Fisher–Yates Shuffle
     function shuffleClass (array) {
         let m = array.length;
         let t;
         let i;
-
         while (m) {
             i = Math.floor(Math.random() * m--);
             t = array[m];
@@ -119,5 +111,23 @@ $(document).ready(function () {
         }
         return array;
     }
-})   
 
+    // // change difficulty
+    // $('#easy').on('click', changeGrid);
+    // $('#med').on('click', changeGrid);
+    // $('#hard').on('click', changeGrid);
+            
+    // function changeGrid(evt) {
+    //     if ($(evt.target).attr('id') === 'easy') {
+    //         $section.attr('class', 'gallery16');
+    //     }
+    //     if ($(evt.target).attr('id') === 'med') {
+    //         $section.attr('class', 'gallery20');
+    //     }
+    //     if ($(evt.target).attr('id') === 'hard') {
+    //         $section.attr('class', 'gallery24');
+    //     }
+    // }
+});  
+        
+        
